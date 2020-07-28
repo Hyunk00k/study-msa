e7ebc7e53b2ff0a837f271e12ac64849255d90e5
hyunkook <bond5hu@gmail.com>
2020-07-28 15:08:24 +0900

    勉強会準備

--- api/startup-api.sh
+++ api/startup-api.sh
@@ -3,4 +3,4 @@
 gradle clean build
 java -jar -Dspring.profiles.active=app-1,local  build/libs/api-0.0.1-SNAPSHOT.jar  &
 java -jar -Dspring.profiles.active=app-2,local  build/libs/api-0.0.1-SNAPSHOT.jar  &
-java -jar -Dspring.profiles.active=app-3,local  build/libs/api-0.0.1-SNAPSHOT.jar  &
+java -jar -Dspring.profiles.active=app-3,local  build/libs/api-0.0.1-SNAPSHOT.jar --service.name="test"  &
